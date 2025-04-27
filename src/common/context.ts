5673081256b5d2484f89c8b9b959322f5253889c
jimzhao <jimzhao@janestreet.com>
2025-04-27 23:11:06 +0800

    use inDebugRepl to disable vim

--- src/common/context.ts
+++ src/common/context.ts
@@ -4,12 +4,17 @@ const LEADERKEY_STATE = "leaderkeyState";
 
 export async function disableVim() {
   // log("[leaderkey] DISABLE VIM");
-  await commands.executeCommand("_setContext", "vim.active", false);
+  // use inDebugRepl to bypass vim keybindings
+  // we don't want to use `vim.active` here, since it might be used by other extensions to
+  // determine the state of vim. debug repl is less likely to cause problems.
+  // await commands.executeCommand("_setContext", "vim.active", false);
+  await commands.executeCommand("_setContext", "inDebugRepl", true);
 }
 
 export async function enableVim() {
   // log("[leaderkey] ENABLE VIM");
-  await commands.executeCommand("_setContext", "vim.active", true);
+  // await commands.executeCommand("_setContext", "vim.active", true);
+  await commands.executeCommand("_setContext", "inDebugRepl", false);
 }
 
 export async function enableLeaderKeyAndDisableVim(path: string) {
