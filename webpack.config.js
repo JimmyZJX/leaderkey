652c4f9351cf51466f2d7c74bcc326634b9620e9
jimzhao <jimzhao@janestreet.com>
2025-05-08 11:11:06 +0800

    fix vsce package with pnpm

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 /** @type WebpackConfig */
 const webExtensionConfig = {
-  mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
-  target: 'webworker', // extensions run in a webworker context
+  mode: "none", // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
+  target: "webworker", // extensions run in a webworker context
   entry: {
-    extension: './src/extension.ts', // source of the web extension main file
+    extension: "./src/extension.ts", // source of the web extension main file
   },
   output: {
-    filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
-    libraryTarget: 'commonjs',
-    devtoolModuleFilenameTemplate: '../[resource-path]'
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+    libraryTarget: "commonjs",
+    devtoolModuleFilenameTemplate: "../[resource-path]",
   },
   resolve: {
-    mainFields: ['browser', 'module', 'main'], // look for `browser` entry point in imported node modules
-    extensions: ['.ts', '.js'], // support ts-files and js-files
+    mainFields: ["browser", "module", "main"], // look for `browser` entry point in imported node modules
+    extensions: [".ts", ".js"], // support ts-files and js-files
     alias: {
       // provides alternate implementation for node module and source files
     },
@@ -25,8 +25,8 @@ const webExtensionConfig = {
       // Webpack 5 no longer polyfills Node.js core modules automatically.
       // see https://webpack.js.org/configuration/resolve/#resolvefallback
       // for the list of Node.js core module polyfills.
-      assert: require.resolve('assert')
-    }
+      assert: require.resolve("assert"),
+    },
   },
   module: {
     rules: [
@@ -35,26 +35,26 @@ const webExtensionConfig = {
         exclude: /node_modules/,
         use: [
           {
-            loader: 'ts-loader',
+            loader: "ts-loader",
             options: {
-              transpileOnly: true
-            }
-          }
-        ]
-      }
-    ]
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new webpack.ProvidePlugin({
-      process: 'process/browser' // provide a shim for the global `process` variable
-    })
+      process: "process/browser", // provide a shim for the global `process` variable
+    }),
   ],
   externals: {
-    vscode: 'commonjs vscode' // ignored because it doesn't exist
+    vscode: "commonjs vscode", // ignored because it doesn't exist
   },
   performance: {
-    hints: false
+    hints: false,
   },
-  devtool: 'nosources-source-map' // create a source map that points to the original source file
+  devtool: "nosources-source-map", // create a source map that points to the original source file
 };
 module.exports = [webExtensionConfig];
